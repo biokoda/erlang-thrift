f96cee05333990ffdb92e543a9f8470c81985836
lamuswawir <muswawir25@gmail.com>
2022-11-05 22:44:12 +0300

    0.10.30 initial commit

---
+++ src/thrift_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc thrift public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(thrift_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    thrift_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
